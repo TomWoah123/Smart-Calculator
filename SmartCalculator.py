3537f8e6bf247a84a106e467bad48e988087e94b
Timothy Wu <timothywu02@gmail.com>
2020-08-13 10:00:11 -0400

    Finished stage 5

--- SmartCalculator.py
+++ SmartCalculator.py
@@ -1,7 +1,15 @@
+# write your code here
+
 def compute(arr):
+    if isInvalidArr(arr):
+        print("Invalid expression")
+        return
     subtract = False
     result = 0
     for thing in arr:
+        if isInvalidExp(thing):
+            print("Invalid expression")
+            return
         if not thing.lstrip('-').isdigit():
             for sign in thing:
                 if sign == '-':
@@ -12,6 +20,39 @@ def compute(arr):
             result -= int(thing)
             subtract = False
     print(result)
+
+
+def isInvalidExp(exp):
+    if not exp.lstrip('+-').isdigit():
+        return True
+    elif not exp[len(exp) - 1].isdigit():
+        return True
+    return False
+
+
+def isOnlyPlus(exp):
+    for sign in exp:
+        if not sign == '+':
+            return False
+    return True
+
+
+def isOnlyMinus(exp):
+    for sign in exp:
+        if not sign == '-':
+            return False
+    return True
+
+
+def isInvalidArr(arr):
+    if isOnlyPlus(arr[len(arr) - 1]) or isOnlyMinus(arr[len(arr) - 1]):
+        return True
+    for i in range(0, len(arr), 2):
+        if i == len(arr) - 1 and isInvalidExp(arr[i]):
+            return True
+        if not arr[i].lstrip('+-').isdigit() and not (isOnlyPlus(arr[i + 1]) or isOnlyMinus(arr[i + 1])):
+            return True
+    return False
 
 
 while True:
@@ -26,6 +67,8 @@ while True:
             print('The program calculates the sum of numbers')
         elif array[0][0] == '/':
             print('Unknown command')
+        elif isInvalidExp(array[0]):
+            print("Invalid expression")
         else:
             print(int(array[0]))
     else:
